550822345f92e7f36caa047f226214d2b64c0b12
Jingchao Yang <jcyangt@gmail.com>
2018-06-12 15:29:27 -0400

    working vertion before adding more att

--- insert2postgres.py
+++ insert2postgres.py
@@ -15,22 +15,24 @@ import psycopg2.extras
 from os import listdir
 from os.path import isfile, join
 
-mypath = 'C:\/Users\/no281\Documents\harVeyTwitter\harvey_twitter_dataset\/02_archive_only\/testSubset'
+mypath = 'C:\\Users\\no281\\Documents\\harVeyTwitter\\harvey_twitter_dataset\\02_archive_only\\testSubset'
 onlyfiles = []
 for f in listdir(mypath):
     if isfile(join(mypath, f)):
         onlyfiles.append(f)
 print(onlyfiles)
 
-# try:
-#     conn = psycopg2.connect("dbname='twitter' user='postgres' host='localhost' password=''")
-# except:
-#     print("I am unable to connect to the database")
-# cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-# sql = "insert into information values (%s,%s, %s, %s,%s)"
-#
-# # Read file and write file are separated
-startpath = 'C:\/Users\/no281\Documents\harVeyTwitter\harvey_twitter_dataset\/02_archive_only\/testSubset'
+try:
+    conn = psycopg2.connect("dbname='harveyTwitts' user='postgres' host='localhost' password='123456'")
+except:
+    print("I am unable to connect to the database")
+
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+sql = "insert into test values (%s, %s, %s, %s, %s, %s)"
+
+# Read file and write file are separated
+startpath = 'C:\\Users\\no281\\Documents\\harVeyTwitter\\harvey_twitter_dataset\\02_archive_only\\testSubset'
 
 pr = onlyfiles
 
@@ -49,25 +51,35 @@ for f in pr:
         # if check == True:
         # if(tweet.endswith(suffix))
 
-        Twt = ''
-
         try:
             if True:
                 # new_tweet = ast.literal_eval(tweet.strip())
                 new_tw = json.loads(tweet)
+                uid, y, final_timestamp = None, [], None
                 for attr, val in new_tw.items():
+
+                    if attr == 'id':
+                        try:
+                            tid = val
+                        except:
+                            print('no id')
+
                     if attr == 'coordinates':
                         try:
                             for x, y in val.items():
+                                print('$$$$$',y)
                                 if y != 'Point':
                                     coord = str(y[1]) + ',' + str(y[0])
                         except:
                             coord = ''
                             break
+
                     if attr == 'text':
-                        tw = codecs.encode(val, 'utf-8')
-                        # tw = tw.replace(",", " ")
-                        # tw = tw.replace("\n", " ")
+                        tw = val
+                        # tw = codecs.encode(val, 'utf-8')
+                        tw = tw.replace(",", " ")
+                        tw = tw.replace("\n", " ")
+
                     if attr == 'created_at':
                         try:
                             clean_timestamp = datetime.strptime(val, '%a %b %d %H:%M:%S +0000 %Y')
@@ -78,24 +90,30 @@ for f in pr:
                         except:
                             print('NO TIME')
                             break
+
                     if attr == 'user':
                         try:
-                            for attr, val in val.iteritems():
+                            for attr, val in val.items():
                                 if attr == 'id':
-                                    Twt = str(val)
+                                    uid = str(val)
                         except:
                             print('Tweet Encode Error!')
                             continue
-                if coord:
-                    data = (Twt, float(y[1]), float(y[0]), final_timestamp, tw)
+
+
+
+                if tid:
+                    if len(y) == 2:
+                        data = (tid, uid, float(y[1]), float(y[0]), final_timestamp, tw)
+                    else:
+                        data = (tid, uid, None, None, final_timestamp, tw)
                     print(data)
-                    # try:
-                    #     cur.execute(sql, data)
-                    #     conn.commit()
-                    # except:
-                    #     print("I can't insert into information")
+                    try:
+                        cur.execute(sql, data)
+                        conn.commit()
+                    except:
+                        print("I can't insert into Test")
         except:
             raise
     doc_read.close()
-# conn.close()
-
+conn.close()
