fa596db0c15bf4e9e65c557f86d5814f19635d18
Jingchao Yang <jcyangt@gmail.com>
2018-06-14 18:17:29 -0400

    simplified

--- analysis/loc_Based.py
+++ analysis/loc_Based.py
@@ -2,7 +2,8 @@
 Created on 6/13/2018
 @author: Jingchao Yang
 """
-from psqlOperations import psqlCollector
+from psqlOperations import queryClean
+from psqlOperations import queryFromDB
 import geograpy
 import re
 
@@ -11,33 +12,23 @@ if __name__ == "__main__":
     tabName = "test"
 
     ############# Location from coordinates
-    # clo_Lat = "tlat"
-    # clo_Lon = "tlon"
-    # data = psqlCollector.get_coorData(dbConnect, tabName, clo_Lat, clo_Lon)
-    # print(data)
+    clo_Lat = "tlat"
+    clo_Lon = "tlon"
+    data = queryFromDB.get_coorData(dbConnect, tabName, clo_Lat, clo_Lon)
+    print(data)
 
-    ############# Location from text
+    ############ Location from text
     clo_Text = "ttext"
-    data = psqlCollector.get_colData_Eng(dbConnect, tabName, clo_Text)
+    data = queryClean.singleColumn_wFilter(dbConnect, tabName, clo_Text)
 
     locations = []
     for row in data:
-        row = re.sub(r'[^\w]', ' ', row)
-        row = re.sub(r'\s+', ' ', row)
-        row = row.strip()
         print(row)
-
         if len(row) > 0:
             places = geograpy.get_place_context(text=row)
             cities = places.cities
+            print(cities)
             locations.append(cities)
 
     print(locations)
 
-
-
-
-    # strr = 'Virginia, United States is where I live'
-    # places = geograpy.get_place_context(text=strr)
-    # con = places.countries
-    # print(con)
--- analysis/wordCount.py
+++ analysis/wordCount.py
@@ -2,14 +2,14 @@
 Created on 6/13/2018
 @author: Jingchao Yang
 """
-from psqlOperations import queryFromDB
+from psqlOperations import queryClean
 
 if __name__ == "__main__":
     dbConnect = "dbname='harveyTwitts' user='postgres' host='localhost' password='123456'"
     tabName = "test"
     cloName = "ttext"
 
-    resuls = queryFromDB.singleColumn_wFilter(dbConnect, tabName, cloName)
+    resuls = queryClean.singleColumn_wFilter(dbConnect, tabName, cloName)
     for i in resuls:
         print(i)
 
