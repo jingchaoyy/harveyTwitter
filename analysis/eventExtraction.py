a30d1bd6e1f6963b425a206b59a8241d9c4ade9d
Jingchao Yang <jcyangt@gmail.com>
2018-06-19 15:46:24 -0400

    initialize event extractor, done testing

---
+++ analysis/eventExtraction.py
@@ -0,0 +1,26 @@
+"""
+Created on 6/19/2018
+@author: Jingchao Yang
+"""
+from psqlOperations import queryClean
+
+
+def eventBack(twList, eList):
+    # twList: original tweet list
+    # eList: list of events
+    twWithEvents = []
+    for tw in twList:
+        for event in eList:
+            if event in tw[1].lower():
+                twWithEvents.append((tw[0], event))
+    return twWithEvents
+
+dbConnect = "dbname='harveyTwitts' user='postgres' host='localhost' password='123456'"
+tabName = "test"
+clo_Text = "ttext"
+data_text = queryClean.singleColumn_wFilter(dbConnect, tabName, clo_Text)
+print('Original English Only Tweets', data_text)
+
+events = ['infection', 'toxic']
+tid_Events = eventBack(data_text, events)
+print(tid_Events)
