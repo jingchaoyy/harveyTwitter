d2a6bb9ba45254643399cf1e80e6bd36d6ce5d43
Jingchao Yang <jcyangt@gmail.com>
2018-06-20 12:29:29 -0400

    adding select * func

--- psqlOperations/queryFromDB.py
+++ psqlOperations/queryFromDB.py
@@ -21,6 +21,31 @@ def remove_emoji(string):
     return emoji_pattern.sub(r'', string)
 
 
+def get_allData(dbc, tbn):
+    """ query all data from a table """
+    conn = None
+    try:
+        conn = psycopg2.connect(dbc)
+        cur = conn.cursor()
+        cur.execute("select * from " + tbn)
+        print("The number of parts from table " + tbn, cur.rowcount)
+        row = cur.fetchone()
+
+        rList = []
+        while row is not None:
+            # print(row)
+            rList.append(row)
+            row = cur.fetchone()
+
+        return rList
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
 def get_colData(dbc, tbn, clo):
     """ query data from a table """
     conn = None
