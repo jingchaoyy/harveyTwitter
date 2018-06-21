62d0e155296262ad83c183af41ab7807b202b2cd
Jingchao Yang <jcyangt@gmail.com>
2018-06-21 13:17:47 -0400

    enableing output for better user experience, done testing

--- analysis/location_from_tweets.py
+++ analysis/location_from_tweets.py
@@ -11,18 +11,18 @@ tb_out_Name = "original"
 ############ Location from text
 clo_Text = "ttext"
 data_text = queryClean.singleColumn_wFilter(dbConnect, tb_out_Name, clo_Text)
-# print('Original English Only Tweets', data_text)
+print('text collecting finished', len(data_text))
 
 setCountry = 'United States'
 locFilter = ['Harvey', 'Hurricane']  # Name list that should not be considered as location under certain event
 loc_fromText = location_tools.locFromText(setCountry, data_text, locFilter)
-# print('All locations extracted', loc_fromText)
+print('loc extraction and filtering finished', len(loc_fromText))
 
 loc_nonDup = location_tools.Remove(loc_fromText)
-# print('Non duplicate location list', loc_nonDup)
+print('remove duplicate loc finished', len(loc_nonDup))
 
 coorFromLoc_nonDup = location_tools.locToCoor(loc_nonDup)
-# print('Associated coordinates', coorFromLoc_nonDup)
+print('coor to loc assign finished', len(coorFromLoc_nonDup))
 
 text_LocCoors = location_tools.coorToTexts(coorFromLoc_nonDup, loc_fromText)
-# print(text_LocCoors)
+print('correlate coor back to all loc found in url finished', len(coorFromLoc_nonDup))
