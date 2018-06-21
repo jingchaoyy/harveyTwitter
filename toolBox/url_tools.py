9626c920ecf97f1d9c4c6f1a823cf8a9dc3dc445
Jingchao Yang <jcyangt@gmail.com>
2018-06-21 12:57:36 -0400

    bug fix, done testing
    location url can be none (user did not include any url in text), adding if to check

--- toolBox/url_tools.py
+++ toolBox/url_tools.py
@@ -3,16 +3,22 @@ Created on 6/20/2018
 @author: Jingchao Yang
 """
 import geograpy
+
+
 def urlFilter(urlList, filterList):
     filteredURL = []
     for url in urlList:
-        if not any(e in url[1] for e in filterList):
+        if url[1] is not None and not any(e in url[1] for e in filterList):
             filteredURL.append(url)
     return filteredURL
+
 
 def findLocFromURL(urlList):
     findLoc = []
     for url in urlList:
         places = geograpy.get_place_context(url=url[1])
         addr = places.address_strings
-        findLoc.append((url[0], addr))
+        print(addr)
+        if len(addr) > 0:
+            findLoc.append((url[0], addr))
+    return findLoc
