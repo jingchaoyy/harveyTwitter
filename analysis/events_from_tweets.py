44e44535c5f3e86964cb4326c36a59119c4e3857
Jingchao Yang <jcyangt@gmail.com>
2018-06-21 16:13:21 -0400

    adding function for url text extraction, done testing

--- analysis/events_from_tweets.py
+++ analysis/events_from_tweets.py
@@ -2,14 +2,38 @@
 Created on 6/19/2018
 @author: Jingchao Yang
 """
+from goose3 import Goose
 
 
 def eventBack(twList, eList):
+    """Extract events from text input, back with events and tid"""
     # twList: original tweet list
     # eList: list of events
     twWithEvents = []
     for tw in twList:
-        for event in eList:
-            if event in tw[1].lower():
-                twWithEvents.append((tw[0], event))
+        if len(tw) > 0:
+            for event in eList:
+                if event in tw[1].lower():
+                    twWithEvents.append((tw[0], event))
     return twWithEvents
+
+
+def textExtractor(urlList):
+    """Extract texts from tweets urls, back with tid with extracted text list"""
+    # urlList: list of urls with tid
+    g = Goose()
+    if urlList:
+        textList = []
+        for url in urlList:
+            try:
+                article = g.extract(url=url[1])
+                text = article.cleaned_text
+                textList.append((url[0], text))
+                # with open(
+                #         r"C:\\Users\\no281\\Documents\\harVeyTwitter\\articalExtracted\\test\\" + str(
+                #             url[0]) + ".txt", 'w') as outfile:
+                #     outfile.write(text)
+                # outfile.close()
+            except:
+                print('url broken, continue')
+    return textList
