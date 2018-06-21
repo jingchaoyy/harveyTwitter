dc02262bba940fb2c50f118444c983e3b173818f
Jingchao Yang <jcyangt@gmail.com>
2018-06-21 13:04:25 -0400

    check if loction list back from  is empty, done testing

--- analysis/location_from_urls.py
+++ analysis/location_from_urls.py
@@ -2,14 +2,45 @@
 Created on 6/20/2018
 @author: Jingchao Yang
 """
-def locationFilter(locList, filterList, region):
+from toolBox import url_tools
+from psqlOperations import queryFromDB
+from toolBox import location_tools
+
+
+def locationFilter(locList, filterList, region):  # filter out location out of certain region
     filteredLoc = []
     for loc in locList:
-        country = loc[1].split(',')[2]
-        if region in country and not any(e in loc[1] for e in filterList):
-            filteredLoc.append(loc)
+        if len(loc) > 0:
+            for l in loc[1]:
+                country = l.split(',')[2]
+                if region in country and not any(e in l for e in filterList):
+                    print((loc[0], l))
+                    filteredLoc.append((loc[0], l))
     return filteredLoc
 
 
-setCountry = 'United States'
-locFilter = ['Harvey', 'Hurricane']
+dbConnect = "dbname='harveyTwitts' user='postgres' host='localhost' password='123456'"
+tb_out_Name = "test"
+clo_url = "teu_url"
+allURLs = queryFromDB.get_colData(dbConnect, tb_out_Name, clo_url)
+print('url collecting finished', len(allURLs))
+
+filters = ['twitter.com', 'youtube.com', 'instagram.com']  # remove links that are from social media
+filteredURLs = url_tools.urlFilter(allURLs, filters)
+print('url filtering finished', len(filteredURLs))
+urlToLoc = url_tools.findLocFromURL(filteredURLs)
+print('loc to url assign finished', len(urlToLoc))
+
+setCountry = 'United States'  # set a region, all location should reside inside
+locFilter = ['Harvey', 'Hurricane', 'Homeland']  # currently not considered as location
+filteredLoc = locationFilter(urlToLoc, locFilter, setCountry)
+print('loc filtering finished', len(filteredLoc))
+
+loc_nonDup = location_tools.Remove(filteredLoc)
+print('remove duplicate loc finished', len(loc_nonDup))
+
+coorFromLoc_nonDup = location_tools.locToCoor(loc_nonDup)
+print('coor to loc assign finished', len(coorFromLoc_nonDup))
+
+text_LocCoors = location_tools.coorToTexts(coorFromLoc_nonDup, filteredLoc)
+print('correlate coor back to all loc found in url finished', len(coorFromLoc_nonDup))
