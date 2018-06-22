0bc878f5354e38be671e0f54af03c59ed34558f9
Jingchao Yang <jcyangt@gmail.com>
2018-06-22 16:45:53 -0400

    adding try to avoid progrem crush

--- geograpy/extraction.py
+++ geograpy/extraction.py
@@ -11,7 +11,6 @@ class Extractor(object):
         self.text = text
         self.url = url
         self.places = []
-    
     def set_text(self):
         if not self.text and self.url:
             a = Article(self.url)
@@ -21,12 +20,15 @@ class Extractor(object):
 
 
     def find_entities(self):
-        self.set_text()
+        try:
+            self.set_text()
 
-        text = nltk.word_tokenize(self.text)
-        nes = nltk.ne_chunk(nltk.pos_tag(text))
+            text = nltk.word_tokenize(self.text)
+            nes = nltk.ne_chunk(nltk.pos_tag(text))
 
-        for ne in nes:
-            if type(ne) is nltk.tree.Tree:
-                if (ne.label() == 'GPE' or ne.label() == 'PERSON' or ne.label() == 'ORGANIZATION'):
-                    self.places.append(u' '.join([i[0] for i in ne.leaves()]))
+            for ne in nes:
+                if type(ne) is nltk.tree.Tree:
+                    if (ne.label() == 'GPE' or ne.label() == 'PERSON' or ne.label() == 'ORGANIZATION'):
+                        self.places.append(u' '.join([i[0] for i in ne.leaves()]))
+        except:
+            print('find_entities failed, continue')
