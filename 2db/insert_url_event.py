1b2a40d35a21434caa6509a07f8caa77ffbfae4f
Jingchao Yang <jcyangt@gmail.com>
2018-06-26 12:57:03 -0400

    expand event list and change query method

--- 2db/insert_url_event.py
+++ 2db/insert_url_event.py
@@ -6,12 +6,16 @@ import psycopg2.extras
 from toolBox import url_tools
 from psqlOperations import queryFromDB
 from dataPreprocessing import events_from_tweets
+import math
 
 dbConnect = "dbname='harveyTwitts' user='postgres' host='localhost' password='123456'"
 tb_in_Name = 'original_urlevents'
-tb_out_Name = "original"
-clo_url = "teu_url"
-allURLs = queryFromDB.get_colData(dbConnect, tb_out_Name, clo_url)
+tb1_out_Name = "original"
+tb2_out_Name = "original_texteng"
+tb1_clo_url = "teu_url"
+tb1_clo_tid = "tid"
+tb2_clo_tid = "tid"
+allURLs = queryFromDB.joinQuery(dbConnect, tb1_out_Name, tb2_out_Name, tb1_clo_url, tb1_clo_tid, tb2_clo_tid)
 print('url collecting finished', len(allURLs))
 
 filters = ['twitter.com', 'youtube.com', 'instagram.com']  # remove links that are from social media
@@ -21,7 +25,22 @@ print('url filtering finished', len(filteredURLs))
 data_text = events_from_tweets.textExtractor(filteredURLs)
 print('extract text from url finished', len(data_text))
 
-events = ['infection', 'toxic', 'rescue', 'power', 'mosquitoes', 'harvey relief', 'donate']
+events = ['infect', 'toxic', 'rescu', 'power', 'mosquito', 'harvey relief', 'donat', 'flood', 'suppl', 'aid',
+          'volunteer', 'high water', 'highwater', 'shelter', 'boat', 'grocery', 'gas', 'highway', 'hwy', 'death',
+          'damage', 'destruction', 'debris', 'casualty', 'caution', 'outage', 'water system', 'devastation', 'leak']
+
+# textListLimit = 10000  # slice the list to smaller pieces to avoid memory issue
+# if len(data_text) > textListLimit:
+#     print("exceeding max query limit", len(data_text))
+#     loops = math.ceil(len(data_text) / textListLimit)
+#     text_Events = []
+#     for j in range(loops):
+#         croopedList = data_text[j * textListLimit:(j + 1) * textListLimit]
+#         text_Event = events_from_tweets.eventBack(croopedList, events)
+#         text_Events += text_Event
+# else:
+#     text_Events = events_from_tweets.eventBack(data_text, events)
+
 text_Events = events_from_tweets.eventBack(data_text, events)
 print('event extraction finished', len(text_Events))
 
