95b79adb0c452a87e2e5732426dca841264e65bc
Jingchao Yang <jcyangt@gmail.com>
2018-07-06 13:36:19 -0400

    update import file name

--- analysis_Credibility/locMatch_with_selfEvaluate.py
+++ analysis_Credibility/locMatch_with_selfEvaluate.py
@@ -2,11 +2,11 @@
 Created on 7/5/2018
 @author: Jingchao Yang
 """
-from dataPreprocessing import location_from_fuzzyMatch
+from dataPreprocessing import gazetteer_from_fuzzyMatch
 from toolBox import fuzzy_gazatteer
 
-self_roadScores = fuzzy_gazatteer.fuzzyLocMatch(location_from_fuzzyMatch.roads_from_tw,
-                                                location_from_fuzzyMatch.roads_from_url)
-self_placeScores = fuzzy_gazatteer.fuzzyLocMatch(location_from_fuzzyMatch.places_from_tw,
-                                                 location_from_fuzzyMatch.places_from_url)
+self_roadScores = fuzzy_gazatteer.fuzzyLocMatch(gazetteer_from_fuzzyMatch.roads_from_tw,
+                                                gazetteer_from_fuzzyMatch.roads_from_url)
+self_placeScores = fuzzy_gazatteer.fuzzyLocMatch(gazetteer_from_fuzzyMatch.places_from_tw,
+                                                 gazetteer_from_fuzzyMatch.places_from_url)
 
