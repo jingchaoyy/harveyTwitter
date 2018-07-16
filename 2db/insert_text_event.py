10385bbba54f0a5b8761f600c5770187da12655e
Jingchao Yang <jcyangt@gmail.com>
2018-07-16 13:02:15 -0400

    major update, done testing
    1. adding merge for tw text extracted events and url extracted events
    2. adding auto drop and create table with rollback func to avoid recreate table that exist

--- 2db/insert_text_event.py
+++ 2db/insert_text_event.py
@@ -5,21 +5,36 @@ Created on 6/19/2018
 import psycopg2.extras
 from psqlOperations import queryFromDB
 
+
+def remove(duList):
+    """
+
+    :param duList:
+    :return:
+    """
+    reList = []
+    for i in duList:
+        if i[0] not in reList:
+            reList.append(i[0])
+    return reList
+
+
 dbConnect = "dbname='harveyTwitts' user='postgres' host='localhost' password='123456'"
+events = ['hotel', 'housing', 'shelter']
 tb_in_Name = 'original_events'
-tb_out_Name = "original_texteng"
-col_Text = "eng_text"
-# data_text = queryClean.singleColumn_wFilter(dbConnect, tb_out_Name, clo_Text)
-# print('Original English Only Tweets', len(data_text))
+tb_out_Name1 = "original_texteng"
+col_Text1 = "eng_text"
+tb_out_Name2 = "original_urltext"
+col_Text2 = "url_text"
 
-events = ['infect', 'toxic', 'rescu', 'power', 'mosquito', 'harvey relief', 'harveyrelief', 'donat', 'flood', 'suppl', 'aid',
-          'volunteer', 'high water', 'highwater', 'shelter', 'boat', 'grocery', 'gas', 'highway', 'hwy', 'death',
-          'damage', 'destruction', 'debris', 'casualty', 'caution', 'outage', 'water system', 'devastation', 'leak']
-# text_Events = events_from_tweets.eventBack(data_text, events)
-# print('event extraction finished', len(text_Events))
+tw_Events = queryFromDB.likeQuery(dbConnect, tb_out_Name1, col_Text1, events)
+print('event extraction from tws finished', len(tw_Events))
 
-text_Events = queryFromDB.likeQuery(dbConnect, tb_out_Name, col_Text, events)
-print('event extraction finished', len(text_Events))
+url_Events = queryFromDB.likeQuery(dbConnect, tb_out_Name2, col_Text2, events)
+print('event extraction from tws finished', len(url_Events))
+
+eList = tw_Events + url_Events
+eList = remove(eList)
 
 try:
     conn = psycopg2.connect("dbname='harveyTwitts' user='postgres' host='localhost' password='123456'")
@@ -28,16 +43,35 @@ except:
 
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-eList = text_Events
+try:
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("drop table " + tb_in_Name)
+    conn.commit()
+    print("drop table succeeded " + tb_in_Name)
+except:
+    print("drop table failed " + tb_in_Name)
+    conn.rollback()  # when command fail, the transaction will be aborted and no further command will be executed
+    # until a call to the rollback(). This except will prevent such abort when table is new and cannot be found and drop
+
+try:
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("create table " + tb_in_Name + "("
+                                               "eID int PRIMARY KEY NOT NULL,"
+                                               "events Text,"
+                                               "tID bigint"
+                                               ");")
+    conn.commit()
+    print("create table succeeded " + tb_in_Name)
+except:
+    print("create table failed " + tb_in_Name)
 
 sql = "insert into " + tb_in_Name + " values (%s, %s, %s)"
-
 for i in range(len(eList)):
-    data = (i, eList[i][1], eList[i][0])
+    data = (i, 'events', eList[i])
     try:
         cur.execute(sql, data)
         conn.commit()
     except:
-        print("I can't insert into" + tb_in_Name)
+        print("I can't insert into " + tb_in_Name)
 
 conn.close()
