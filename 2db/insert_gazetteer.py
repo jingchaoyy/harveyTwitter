76615b4dd91d440fd87ff6d7de1eb88a0bb0ff33
Jingchao Yang <jcyangt@gmail.com>
2018-07-16 14:49:09 -0400

    add auto table drop and create

--- 2db/insert_gazetteer.py
+++ 2db/insert_gazetteer.py
@@ -27,21 +27,21 @@ def collectTID(list):
 
 ''' db connection '''
 dbConnect = "dbname='harveyTwitts' user='postgres' host='localhost' password='123456'"
-tb_in_Name = 'test_gazetteer'
+tb_in_Name = 'original_gazetteer'  # table to store gazetteers, and associated ground truth match score with tid
 try:
     conn = psycopg2.connect("dbname='harveyTwitts' user='postgres' host='localhost' password='123456'")
 except:
     print("I am unable to connect to the database")
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-''' fuzzy gazetteers '''
+''' fuzzy gazetteers, getting gazetteers from text (tws/ urls) '''
 roads_from_tw = gazetteer_from_fuzzyMatch.roads_from_tw
 roads_from_url = gazetteer_from_fuzzyMatch.roads_from_url
 places_from_tw = gazetteer_from_fuzzyMatch.places_from_tw
 places_from_url = gazetteer_from_fuzzyMatch.places_from_url
 allGazetteers = [roads_from_tw, places_from_tw, roads_from_url, places_from_url]
 
-''' scores '''
+''' match scores between gazetteers from text and ground truth '''
 roads_from_tru = gazetteer_from_fuzzyMatch.roads_from_tru
 places_from_tru = gazetteer_from_fuzzyMatch.places_from_tru
 
@@ -50,6 +50,33 @@ roadScores_url = fuzzy_gazatteer.fuzzyLocMatch_wGT(roads_from_url, roads_from_tr
 placeScores_tw = fuzzy_gazatteer.fuzzyLocMatch_wGT(places_from_tw, places_from_tru)
 placeScores_url = fuzzy_gazatteer.fuzzyLocMatch_wGT(places_from_url, places_from_tru)
 allScores = [roadScores_tw, placeScores_tw, roadScores_url, placeScores_url]
+
+''' check is tb exist, if yes, drop and recreate '''
+try:
+    cur.execute("drop table " + tb_in_Name)
+    conn.commit()
+    print("drop table succeeded " + tb_in_Name)
+except:
+    print("drop table failed " + tb_in_Name)
+    conn.rollback()  # when command fail, the transaction will be aborted and no further command will be executed
+    # until a call to the rollback(). This except will prevent such abort when table is new and cannot be found and drop
+
+try:
+    cur.execute("create table " + tb_in_Name + "(eID int PRIMARY KEY NOT NULL,"
+                                               "tw_road Text,"
+                                               "tw_place Text,"
+                                               "url_road Text,"
+                                               "url_place Text,"
+                                               "tw_road_score double precision,"
+                                               "tw_place_score double precision,"
+                                               "url_road_score double precision,"
+                                               "url_place_score double precision,"
+                                               "tcreate Text,"
+                                               "tID bigint);")
+    conn.commit()
+    print("create table succeeded " + tb_in_Name)
+except:
+    print("create table failed " + tb_in_Name)
 
 ''' insert all tid into the table first, can be used as key for later table update '''
 all_tid = collectTID(allScores)
@@ -75,7 +102,6 @@ for gazetteers in allGazetteers:
 
 ''' update table with scores based on tid '''
 score_colNames = ['tw_road_score', 'tw_place_score', 'url_road_score', 'url_place_score']  # match with allScores
-
 for scoreset in allScores:
     for score in scoreset:
         print("update " + tb_in_Name + " set " + score_colNames[
@@ -87,13 +113,12 @@ for scoreset in allScores:
         except:
             print("I can't insert score into " + tb_in_Name)
 
-''' update table with post time based on tid '''
-tw_tb = "test"
+''' update table with tweet time based on tid '''
+tw_tb = "original"
 tb_col = "tcreate"
 post_time = queryFromDB.get_colData(dbConnect, tw_tb, tb_col)
 
 time_col = 'tcreate'
-
 for time in post_time:
     print("update " + tb_in_Name + " set " + time_col + " = '" + str(time[1])
           + "' where tid = " + str(time[0]))
