7948051925195bd96e762d90ac80712dc54eab4f
Jingchao Yang <jcyangt@gmail.com>
2018-07-19 17:13:15 -0400

    small bug fix, add comments

--- analysis_Credibility/eventBased_selfEvaluate.py
+++ analysis_Credibility/eventBased_selfEvaluate.py
@@ -9,15 +9,25 @@ dbConnect = "dbname='harveyTwitts' user='postgres' host='localhost' password='12
 
 
 def selfCrediable(tw_gz, url_gz):
+    """
+    Check if same extracted gazetteer shows both in user tweets and in urls, if yes, give credits
+    Based on original_gazetteer table and compare with original_credibility_improved table, since corelated roads
+    and places are already grouped  using geocoding in original_credibility_improved table.
+
+    :param tw_gz: gazetteer from tws
+    :param url_gz: gazetteer from urls
+    :return: credibility after self evaluated
+    """
     tb_out_Name = "original_credibility_improved"
     tb_query_Col = 'tids'
     creditList = []
     for tgz in tw_gz:
         credit = 0
-        if tgz[0] != ', ':
+        if tgz[0] != ', ':  # credit is 0 when no matches, no need to go though the rest
             gzs = events_from_tweets.remove(tgz[0].split(','))
             tid = [str(tgz[-1])]
-            events = queryFromDB.likeQuery_all(dbConnect, tb_out_Name, tb_query_Col, tid)
+            events = queryFromDB.likeQuery_all(dbConnect, tb_out_Name, tb_query_Col, tid)  # get data from
+            # original_credibility_improved table, work as a local gazetteer db, collect eid as gazetteer reference No.
             eids = []
             for event in events:
                 for gz in gzs:  # for each gz under same tid
@@ -30,8 +40,8 @@ def selfCrediable(tw_gz, url_gz):
                             if gz in event[2]:
                                 eids.append(event[0])
 
-            eids = events_from_tweets.remove(eids)
-            if len(eids) > 0:
+            eids = events_from_tweets.remove(eids)  # keep only non duplicated gazetteer reference No. (eids)
+            if len(eids) > 0:  # when matches found in user tweets, meaning there's a chance same location also in urls
                 events_1 = []
                 for event in events:
                     if event[0] in eids:
@@ -50,7 +60,9 @@ def selfCrediable(tw_gz, url_gz):
                                     if gz.startswith('#p '):
                                         if gz in event1[2]:
                                             eids.append(event1[0])
-                credit = len(eids) - len(events_from_tweets.remove(eids))
+                credit = len(
+                    set([i for i in eids if eids.count(i) > 1]))  # get eids that shown in the list more than once,
+                # meaning found both in tw and in url
         print((credit, tgz[-1]))
         creditList.append((credit, tgz[-1]))
     return creditList
