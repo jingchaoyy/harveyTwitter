cfdb030deaa4b584004f8066084c5fcacae96156
Jingchao Yang <jcyangt@gmail.com>
2018-07-19 16:36:29 -0400

    code reorganize, better performence

--- analysis_Credibility/locMatch_with_selfEvaluate.py
+++ analysis_Credibility/locMatch_with_selfEvaluate.py
@@ -14,44 +14,45 @@ def selfCrediable(tw_gz, url_gz):
     creditList = []
     for tgz in tw_gz:
         credit = 0
-        tid = [str(tgz[-1])]
-        events = queryFromDB.likeQuery_all(dbConnect, tb_out_Name, tb_query_Col, tid)
-        eids = []
-        for event in events:
+        if tgz[0] != ', ':
             gzs = events_from_tweets.remove(tgz[0].split(','))
-            for gz in gzs:  # for each gz under same tid
-                gz = gz.strip()
-                if gz != '':
-                    if gz.startswith('*R '):
-                        if gz in event[1]:
-                            eids.append(event[0])
-                    if gz.startswith('#p '):
-                        if gz in event[2]:
-                            eids.append(event[0])
-
-        eids = events_from_tweets.remove(eids)
-        if len(eids) > 0:
-            events_1 = []
+            tid = [str(tgz[-1])]
+            events = queryFromDB.likeQuery_all(dbConnect, tb_out_Name, tb_query_Col, tid)
+            eids = []
             for event in events:
-                if event[0] in eids:
-                    events_1.append(event)
+                for gz in gzs:  # for each gz under same tid
+                    gz = gz.strip()
+                    if gz != '':
+                        if gz.startswith('*R '):
+                            if gz in event[1]:
+                                eids.append(event[0])
+                        if gz.startswith('#p '):
+                            if gz in event[2]:
+                                eids.append(event[0])
 
-            for ugz in url_gz:
-                if ugz[-1] == tgz[-1]:
-                    for event1 in events_1:
-                        gzs = events_from_tweets.remove(ugz[0].split(','))
-                        for gz in gzs:  # for each gz under same tid
-                            gz = gz.strip()
-                            if gz != '':
-                                if gz.startswith('*R '):
-                                    if gz in event1[1]:
-                                        eids.append(event1[0])
-                                if gz.startswith('#p '):
-                                    if gz in event1[2]:
-                                        eids.append(event1[0])
-            credit = len(eids) - len(events_from_tweets.remove(eids))
-        print(credit)
-        creditList.append(credit)
+            eids = events_from_tweets.remove(eids)
+            if len(eids) > 0:
+                events_1 = []
+                for event in events:
+                    if event[0] in eids:
+                        events_1.append(event)
+
+                for ugz in url_gz:
+                    if ugz[-1] == tgz[-1]:
+                        for event1 in events_1:
+                            gzs = events_from_tweets.remove(ugz[0].split(','))
+                            for gz in gzs:  # for each gz under same tid
+                                gz = gz.strip()
+                                if gz != '':
+                                    if gz.startswith('*R '):
+                                        if gz in event1[1]:
+                                            eids.append(event1[0])
+                                    if gz.startswith('#p '):
+                                        if gz in event1[2]:
+                                            eids.append(event1[0])
+                credit = len(eids) - len(events_from_tweets.remove(eids))
+        print((credit, tgz[-1]))
+        creditList.append((credit, tgz[-1]))
     return creditList
 
 
@@ -66,5 +67,5 @@ col4 = "url_place"
 tw_gz1 = queryFromDB.mergeSelect(dbConnect, gz_tb, col1, col2)
 url_gz1 = queryFromDB.mergeSelect(dbConnect, gz_tb, col3, col4)
 evaluate_score = selfCrediable(tw_gz1, url_gz1)
-for i in evaluate_score:
-    print(i)
+# for i in evaluate_score:
+#     print(i)
