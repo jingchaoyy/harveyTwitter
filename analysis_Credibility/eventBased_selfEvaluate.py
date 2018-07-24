f90f221863bfcf2d65ed73cecd19b0b59720a46f
Jingchao Yang <jcyangt@gmail.com>
2018-07-24 16:46:19 -0400

    minor update

--- analysis_Credibility/eventBased_selfEvaluate.py
+++ analysis_Credibility/eventBased_selfEvaluate.py
@@ -50,41 +50,40 @@ def assignCredit(events):
             gazetteer = gazetteer[0]
             if gazetteer[1] is not None:
                 tw_roads = events_from_tweets.remove(gazetteer[1].split(', '))
-                print(tw_roads)
+                # print(tw_roads)
             if gazetteer[2] is not None:
                 tw_places = events_from_tweets.remove(gazetteer[2].split(', '))
-                print(tw_places)
+                # print(tw_places)
             if gazetteer[3] is not None:
                 url_roads = events_from_tweets.remove(gazetteer[3].split(', '))
-                print(url_roads)
+                # print(url_roads)
             if gazetteer[4] is not None:
                 url_places = events_from_tweets.remove(gazetteer[4].split(', '))
-                print(url_places)
+                # print(url_places)
 
             ''' see if any roads from tw matches, if not, check places. Then for the urls
             For each tid, max location match score is 1 + 1 = 2 '''
             if any(twr in roadEvents for twr in tw_roads):
                 locCredit.append(1)
-                print('aaaa')
+                # print('aaaa')
             elif any(twp in placeEvents for twp in tw_places):
                 locCredit.append(1)
-                print('bbbb')
+                # print('bbbb')
 
             if any(urlr in roadEvents for urlr in url_roads):
                 locCredit.append(1)
-                print('cccc')
+                # print('cccc')
             elif any(urlp in placeEvents for urlp in url_places):
                 locCredit.append(1)
-                print('dddd')
+                # print('dddd')
 
-        print((eid, sum(locCredit) / len(resourceType), sum(rtCredit)))
+        print((eid, sum(locCredit) / len(resourceType), sum(rtCredit) / maxRT))
         eventCredits.append((eid, sum(locCredit) / len(resourceType), sum(rtCredit) / maxRT))
+    return eventCredits
 
 
 tb1_out_Name = "original_credibility_improved"
 colList1 = ['eid', 'road_events', 'place_events', 'tids']
 
 data_events = queryFromDB.get_multiColData(dbConnect, tb1_out_Name, colList1)
-# for i in data_events:
-#     print(i)
-assignCredit(data_events)
+creditList = assignCredit(data_events)
