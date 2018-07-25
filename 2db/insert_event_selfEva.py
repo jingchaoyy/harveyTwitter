0c085ede4c0eeb07d9ad8a31f39b82d0c84cdc0f
Jingchao Yang <jcyangt@gmail.com>
2018-07-25 11:30:12 -0400

    table update bug fix

--- 2db/insert_event_selfEva.py
+++ 2db/insert_event_selfEva.py
@@ -19,16 +19,14 @@ cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 try:
     cur.execute("alter table " + tb_in_Name + " drop loc_Credit, drop rt_Credit;")
     conn.commit()
-    print("drop table column succeeded " + tb_in_Name)
+    print("alter table column succeeded " + tb_in_Name)
 except:
-    print("drop table failed " + tb_in_Name)
+    print("alter table failed " + tb_in_Name)
     conn.rollback()  # when command fail, the transaction will be aborted and no further command will be executed
     # until a call to the rollback(). This except will prevent such abort when table is new and cannot be found and drop
 
 try:
-    cur.execute("alter table " + tb_in_Name + " add loc_Credit double precision,"
-                                              " add rt_Credit double precision"
-                                              ";")
+    cur.execute("alter table " + tb_in_Name + " add loc_Credit double precision, add rt_Credit double precision;")
     conn.commit()
     print("alter table succeeded " + tb_in_Name)
 except:
@@ -37,8 +35,8 @@ except:
 eList = eventBased_selfEvaluate.creditList
 for i in range(len(eList)):
     try:
-        cur.execute("update " + tb_in_Name + " set loc_Credit = " + str(eList[i][1])
-                    + ", set rt_Credit = " + str(eList[i][2]) + " where eid = " + str(eList[i][0]))
+        cur.execute("update " + tb_in_Name + " set loc_Credit = " + str(eList[i][1]) + ", rt_Credit = " +
+                    str(eList[i][2]) + " where eid = " + str(eList[i][0]))
         conn.commit()
     except:
         print("I can't insert into " + tb_in_Name)
