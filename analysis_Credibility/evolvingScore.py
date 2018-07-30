0edc1a865122887ff5e0f843c7069036eb2af190
Jingchao Yang <jcyangt@gmail.com>
2018-07-30 15:57:32 -0400

    complete with credit accumulation plot

--- analysis_Credibility/evolvingScore.py
+++ analysis_Credibility/evolvingScore.py
@@ -8,7 +8,6 @@ Collect tids under certain event and locate their
 from psqlOperations import queryFromDB
 import matplotlib.pyplot as plt
 import pandas as pd
-import datetime
 
 dbConnect = "dbname='harveyTwitts' user='postgres' host='localhost' password='123456'"
 
@@ -31,45 +30,114 @@ def getEvlScore(tidList):
     return timeLine
 
 
-def getTIDs(eid):
+def getData(col, eid):
     """
     Input event id from credibility table, and return a list of supporting tids
 
+    :param col: column name
     :param eid: event id
-    :return: list of associated
+    :return: list of associated data
     """
     tb_out_Name = "original_credibility_improved"
-    sql = "select tids from " + tb_out_Name + " where eid = '" + str(eid) + "'"
-    tids = queryFromDB.freeQuery(dbConnect, sql)[0][0]
-    tids = tids.split(', ')
-    print('Supporting tweet IDs:', tids)
-    return tids
+    sql = "select " + col + " from " + tb_out_Name + " where eid = '" + str(eid) + "'"
+    data = queryFromDB.freeQuery(dbConnect, sql)[0][0]
+    if isinstance(data, str):
+        data = data.split(', ')
+    print(col, data)
+    return data
 
 
 def dateCount(dList):
+    """
+    Will simply count twitter numbers for each date
+
+    :param dList: date list
+    :return: a pandas df with sorted dates and associated twitter counts
+    """
     dateList, count = [], []
-    for date in dList:
-        if date not in dateList:
-            dateList.append(date)
+    for date in range(len(dList)):
+        if dList[date] not in dateList:
+            dateList.append(dList[date])
             count.append(1)
         else:
-            ind = dateList.index(date)
+            ind = dateList.index(dList[date])
             count[ind] = count[ind] + 1
 
     data = {'date': dateList, 'count': count}
     df = pd.DataFrame(data)
     df = df.sort_values('date', ascending=True)
     print(df)
-    plt.plot(df['date'], df['count'])
-    plt.xticks(rotation='vertical')
-    plt.show()
+    return df
 
 
-supTIDs = getTIDs(13)
+def dateCredit(dList, locCreitList, rtCreditList):
+    """
+    Allowing credit accumulation.
+
+    :param dList: date list
+    :param locCreitList: list of event match credit
+    :param rtCreditList: list of retweet credit
+    :return: two pandas dfs, one simply with dates and credits collected for associated days. Another is with evolving
+    credits calculated
+    """
+    dateList, creditList = [], []
+    for date in range(len(dList)):
+        credit = locCreitList[date] + rtCreditList[date]
+        if dList[date] not in dateList:
+            dateList.append(dList[date])
+            creditList.append(credit)
+        else:
+            ind = dateList.index(dList[date])
+            creditList[ind] = creditList[ind] + credit
+
+    data = {'date': dateList, 'credit': creditList}
+    df = pd.DataFrame(data)
+    df = df.sort_values('date', ascending=True)
+    print(df)
+
+    df_evl = df
+    df_evl = df_evl.assign(sum_credit=df_evl.credit.cumsum())
+    print(df_evl)
+
+    return df, df_evl
+
+
+eid = 5536
+supTIDs = getData("tids", eid)
 timeList = getEvlScore(supTIDs)
 dt = pd.to_datetime(timeList)  # from 12h convert to 24h, and using pandas datetime object
 dates = dt.date
 hours = dt.hour
 
-''' Plot '''
-# dateCount(dates)  # plot by date count
+''' organize by date count'''
+count_df = dateCount(dates)
+
+''' organize by credibility '''
+supLocCredits = getData("loc_credits", eid)
+supRTCredits = getData("rt_credits", eid)
+credit_df, credit_df_evl = dateCredit(dates, supLocCredits, supRTCredits)
+
+ax1 = plt.subplot(311)
+plt.plot(count_df['date'], count_df['count'])
+plt.xticks(rotation='vertical')
+plt.title('Twitter Count Plot')
+plt.ylabel('Total Tweets Number')
+# plt.xlabel('Dates')
+
+ax2 = plt.subplot(312, sharex=ax1)
+plt.plot(credit_df['date'], credit_df['credit'])
+plt.xticks(rotation='vertical')
+plt.title('Credibility Evolving Plot')
+plt.ylabel('Credibility Score')
+# plt.xlabel('Dates')
+
+ax3 = plt.subplot(313, sharex=ax1)
+plt.plot(credit_df_evl['date'], credit_df_evl['sum_credit'])
+plt.xticks(rotation='vertical')
+plt.title('Credibility Evolving Plot')
+plt.ylabel('Credibility Accumulation')
+plt.xlabel('Dates')
+
+plt.setp(ax1.get_xticklabels(), visible=False)
+plt.setp(ax2.get_xticklabels(), visible=False)
+plt.show()
