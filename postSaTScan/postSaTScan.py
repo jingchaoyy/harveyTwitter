c8de480f9c01abca30a3e7e2e668153008d6b4f2
Jingchao Yang <jcyangt@gmail.com>
2019-02-08 08:12:54 -0500

    involving non cluster IDs. Done

--- postSaTScan/postSaTScan.py
+++ postSaTScan/postSaTScan.py
@@ -7,8 +7,9 @@ from psqlOperations import queryFromDB
 
 dbConnect = "dbname='harveyTwitts' user='postgres' host='localhost' password='123456'"
 tb_in = 'original_gazetteer_power'
-path = 'D:\\harveyTwitter\\SaTScan_HouBry\\result\\result-Copy.txt'
-path_csv = 'D:\\harveyTwitter\\power_distribution_old_2.txt'
+path_withClusters = 'D:\\harveyTwitter\\SaTScan_HouBry\\result\\result-Copy.txt'
+path_withTIDs = 'D:\\harveyTwitter\\power_distribution_old_2.txt'
+path_allIDs = 'D:\\harveyTwitter\\power_Clip_old2_HouBry_TableToExcel.csv'
 
 
 def getCluster(dpath):
@@ -28,6 +29,24 @@ def getCluster(dpath):
         clusters.append((id, cluster))
         id += 1
     return clusters
+
+
+def matchIDs(dpath, orgCluster):
+    """
+
+    :param dpath:
+    :return:
+    """
+    data = pd.read_csv(dpath, header=None)
+    # print(data)
+    allClusters = []
+    for c in orgCluster:
+        tids = []
+        for i in c[1]:
+            tid = data.loc[data[0] == int(i) - 1]
+            tids.append(str(tid.iloc[0][1]))
+        allClusters.append((c[0], tids))
+    return allClusters
 
 
 def matchTIDs(dpath, orgCluster):
@@ -61,19 +80,47 @@ def matchGazetteer(col, tidList):
 
 
 '''get clusters'''
-cluster_list = getCluster(path)
-# for c in cluster_list:
+cluster_list = getCluster(path_withClusters)
+allID_in_cluster = []
+for cl in cluster_list:
+    allID_in_cluster += cl[1]
+allID_in_cluster = set(allID_in_cluster)
+print('allID belong to cluster', allID_in_cluster)
+
+'''get all IDs'''
+allIDs = pd.read_csv(path_allIDs, header=None)
+allIDs = allIDs[0]
+IDList = allIDs.tolist()
+IDList = [str(i + 1) for i in IDList]
+IDSet = set(IDList)
+print('all ID inputs', IDSet)
+
+'''get non-clustered IDs'''
+nonCluster_IDs = IDSet - allID_in_cluster
+nonCluster_IDs = list(nonCluster_IDs)
+print(len(IDSet), len(allID_in_cluster), len(nonCluster_IDs))
+clusterID = len(cluster_list)
+for nc in nonCluster_IDs:
+    cluster_list.append((clusterID, [nc]))
+    clusterID += 1
+
+'''match IDs'''
+print('start match location IDs to original IDs')
+cluster_list1 = matchIDs(path_allIDs, cluster_list)
+# for c in cluster_list1:
 #     print(c)
 
 '''match to original tids'''
-cluster_list2 = matchTIDs(path_csv, cluster_list)
-for c in cluster_list2:
-    print(c)
+print('start match original IDs to tids')
+cluster_list2 = matchTIDs(path_withTIDs, cluster_list1)
+# for c in cluster_list2:
+#     print(c)
 
 '''match tids with gazetteer table'''
+print('start match tids with location names')
 gazetteer_list = []
-for cl in cluster_list2:
-    gazetteer = matchGazetteer('tw_road,tw_place,url_road,url_place', cl[1])
+for cl2 in cluster_list2:
+    gazetteer = matchGazetteer('tw_road,tw_place,url_road,url_place', cl2[1])
     duplicate, joint_gazetteer = [], []
     for sub in gazetteer:
         for subsub in sub:
@@ -88,8 +135,5 @@ for cl in cluster_list2:
                 if loc is not None and loc not in duplicate:
                     joint_gazetteer.append(loc)
                     duplicate.append(loc)
-
-    gazetteer_list.append((cl[0], joint_gazetteer))
-
-for gl in gazetteer_list:
-    print(gl)
+    print((cl2[0], joint_gazetteer))
+    gazetteer_list.append((cl2[0], joint_gazetteer))
